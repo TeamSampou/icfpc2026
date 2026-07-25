75cc1e39eded668e676c97b2509aebef025b6002
Kei Hibino <ex8k.hibino@gmail.com>
2026-07-25 16:40:40 +0900

    json-files: actions to load problems from JSON files

--- src/FilesJSON.hs
+++ src/FilesJSON.hs
@@ -4,6 +4,7 @@ module FilesJSON where
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import System.FilePath ((</>), (<.>))
+import System.IO.Error
 
 import Data.Aeson
 
@@ -14,9 +15,29 @@ loadProbStatList = do
   bs <- L8.readFile "list-problem.json"
   either fail pure $ eitherDecode bs
 
+type PSlug = String
+
+problemPath :: PSlug -> FilePath
+problemPath pslug = "problems" </> pslug <.> "json"
+
 getDownloadCmdLines :: IO [String]
 getDownloadCmdLines = do
   pss <- loadProbStatList
   pure [cmd $ pstatSlug ps | ps <- pss]
   where
-    cmd pslug = unwords ["./api/fetch-prob.sh", pslug, ">", "problems" </> pslug <.> "json"]
+    cmd pslug = unwords ["./api/fetch-prob.sh", pslug, ">", problemPath pslug]
+
+loadProblem :: PSlug -> IO Problem
+loadProblem pslug = do
+  bs <- L8.readFile $ problemPath pslug
+  either (\e -> fail (pslug ++ ":" ++ e)) pure $ eitherDecode bs
+
+loadProblemList' :: IO [Either IOError Problem]
+loadProblemList' = do
+  pss <- loadProbStatList
+  sequence [tryIOError $ loadProblem $ pstatSlug ps | ps <- pss]
+
+loadProblemList :: IO [Problem]
+loadProblemList = do
+  pss <- loadProbStatList
+  sequence [loadProblem $ pstatSlug ps | ps <- pss]
