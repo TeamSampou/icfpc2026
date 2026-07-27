99ef2a737cb1b1ac5ca28e4be0e8e0cf6ac19b52
Nobuo Yamashita <nobsun@sampou.org>
2026-07-27 20:44:10 +0900

    update history-lesson

--- app/history-lesson/Main.hs
+++ app/history-lesson/Main.hs
@@ -3,47 +3,46 @@ module Main
     )
     where
 
-import Data.Bits
-import Data.Char
-import Data.List
-import Data.Ord
-import Data.List.Split
-import Text.Printf
+import Data.Char ( ord, intToDigit )
+import Data.List.Split ( chunksOf )
 import Exam ( history )
 
 main :: IO ()
-main = do
-    putStr mkHistory
-    -- print $ length $ encs history
-    -- mapM_ print $ encs history
-
-cn :: Int
-cn = 22
-
-encs :: String -> [String]
-encs = list [] psi . map concat . chunksOf cn . ("`32` W" :) . map phi
-    where
-        phi :: Char -> String
-        phi c   = printf "`%02d`+S" (ord c - 32)
-        psi :: String -> [String] -> [String]
-        psi xs xss = ("|@ "++init xs++"v|") : zipWith id (cycle [f,g]) xss
-                where
-                    f s | length s == cn * 6 = "|v" ++ drop 1 (reverse s) ++ "S<|"
-                        | otherwise          = "|v" ++  replicate (cn*6 - length s - 1) ' '  ++ 'S' : drop 1 (reverse s) ++ "S<|"
-                    g s | length s == cn * 6 = "|>S" ++ init s ++ "v|"
-                        | otherwise          = "|>S" ++ init s ++ 'S' : replicate (cn*6 - length s - 1) ' ' ++  "v|"
+main = putStr mkLesson
         
+cn :: Int
+cn = 105
+
 list :: b -> (a -> [a] -> b) -> [a] -> b
 list z f xs = case xs of
     []   -> z
     y:ys -> f y ys
 
-mkHistory :: String
-mkHistory 
-    = unlines
-    $  [tbline]
-    ++ encs history
-    ++ [tbline]
-    ++ [" ^  v"," ^  v","+-++-+","|I||O|","+-++-+"]
+conv :: Char -> (Int,Int)
+conv c = (ord c - 32) `divMod` 10
+
+send :: Char -> String
+send c = case conv c of
+    (i,j) | i == j    -> intToDigit i : "SS"
+          | otherwise -> intToDigit i : 'S' : intToDigit j : "S"
+
+hists :: [String]
+hists = list [] phi 
+      $ chunksOf cn $ concatMap send history
     where
-        tbline = "+" ++ replicate (6*cn+2) '-' ++ "+"
+        phi xs xss = ("|@"++xs++"v|") : zipWith id (cycle [f,g]) xss
+        f xs = "|v"++reverse (take cn (xs ++ repeat ' '))++"<|"
+        g xs = "|>"++         take cn (xs ++ repeat ' ') ++"v|"
+
+mkLesson :: String
+mkLesson = unlines
+         $  [ubline]
+         ++  hists
+         ++ [ubline]
+         ++ ["^         +---------------+v"
+            ,"^<+-++-+  |>@RW`10`*W`32`v|<"
+            ,"  |I||O|<<|^S+R`  `W+`  `<|"
+            ,"  +-++-+  +---------------+"
+            ]
+    where
+        ubline = "+"++replicate (cn+2) '-'++"+"
